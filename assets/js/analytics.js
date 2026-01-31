99b99016a2759374fe35903f1032d7e9fb7f2d77
nit-repo <nitishkennus@gmail.com>
2026-01-31 19:39:24 +0530

    Analytics v1

---
+++ assets/js/analytics.js
@@ -0,0 +1,75 @@
+(function () {
+  'use strict';
+
+  // Exact GA / GTM snippets (kept verbatim)
+  var headSnippet = '<!-- Google tag (gtag.js) -->\n' +
+'<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y2P3RZNXLR"></script>\n' +
+'<script>\n' +
+'  window.dataLayer = window.dataLayer || [];\n' +
+'  function gtag(){dataLayer.push(arguments);}\n' +
+"  gtag('js', new Date());\n\n" +
+"  gtag('config', 'G-Y2P3RZNXLR');\n" +
+'</script>\n' +
+'<!-- Google Tag Manager -->\n' +
+'<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n' +
+'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n' +
+"j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
+"'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
+"})(window,document,'script','dataLayer','GTM-WVFK4S5C');</script>\n" +
+'<!-- End Google Tag Manager -->';
+
+  var bodySnippet = '<!-- Google Tag Manager (noscript) -->\n' +
+'<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVFK4S5C"\n' +
+'height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>\n' +
+'<!-- End Google Tag Manager (noscript) -->';
+
+  // Avoid duplicate insertion
+  function alreadyInserted() {
+    return !!document.querySelector('script[src*="gtag/js?id=G-Y2P3RZNXLR"]') ||
+           !!document.querySelector('script[src*="gtm.js?id=GTM-WVFK4S5C"]') ||
+           !!document.querySelector('noscript iframe[src*="GTM-WVFK4S5C"]') ||
+           !!document.querySelector('script[data-analytics-injected="1"]');
+  }
+
+  if (!alreadyInserted()) {
+    // 1) Ensure gtag external script loads (createElement to guarantee execution)
+    if (!document.querySelector('script[src*="gtag/js?id=G-Y2P3RZNXLR"]')) {
+      var s1 = document.createElement('script');
+      s1.async = true;
+      s1.src = 'https://www.googletagmanager.com/gtag/js?id=G-Y2P3RZNXLR';
+      document.head.insertBefore(s1, document.head.firstChild);
+    }
+
+    // 2) Add inline gtag config (executed immediately)
+    var inlineGtag = document.createElement('script');
+    inlineGtag.text = "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-Y2P3RZNXLR');";
+    document.head.insertBefore(inlineGtag, document.head.firstChild);
+
+    // 3) Add GTM bootstrap (executes and injects the GTM script)
+    var inlineGtm = document.createElement('script');
+    inlineGtm.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WVFK4S5C');";
+    document.head.insertBefore(inlineGtm, document.head.firstChild);
+
+    // Mark injected to avoid duplicates by subsequent loads
+    var marker = document.createElement('script');
+    marker.setAttribute('data-analytics-injected', '1');
+    marker.type = 'text/plain';
+    document.head.insertBefore(marker, document.head.firstChild);
+  }
+
+  // Insert noscript iframe inside <body> immediately after opening <body>
+  function insertNoscript() {
+    if (!document.querySelector('noscript iframe[src*="GTM-WVFK4S5C"]')) {
+      if (document.body) {
+        document.body.insertAdjacentHTML('afterbegin', bodySnippet);
+      } else {
+        document.addEventListener('DOMContentLoaded', function () {
+          document.body.insertAdjacentHTML('afterbegin', bodySnippet);
+        });
+      }
+    }
+  }
+
+  insertNoscript();
+
+})();
--- assets/js/render-products.js
+++ assets/js/render-products.js
@@ -1,4 +1,4 @@
-/* Render product grid from ./data/products.json */
+/* Render product grid from /data/products.json */
 $(function () {
   var $grid = $('#product-grid');
   if (!$grid.length) return;
@@ -8,20 +8,20 @@ $(function () {
     .then(function (products) {
       var html = '';
       products.forEach(function (p) {
-        html += '<div class="col-md-4">';
+        html += '<div class="col-md-4" data-item-id="' + p.id + '" data-item-name="' + (p.title && p.title.replace(/"/g, '&quot;')) + '">';
         html += '  <div class="card mb-4 product-wap rounded-0 h-100">';
         html += '    <div class="card rounded-0">';
-        html += '      <a href="shop-single.html?id=' + p.id + '"><img class="card-img rounded-0 img-fluid" src="' + p.image + '" loading="lazy"></a>';
+        html += '      <a href="shop-single.html?id=' + p.id + '" data-item-id="' + p.id + '" data-item-name="' + (p.title && p.title.replace(/"/g, '&quot;')) + '"><img class="card-img rounded-0 img-fluid" src="' + p.image + '" loading="lazy" alt="' + p.title + '"></a>';
         html += '      <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">';
         html += '        <ul class="list-unstyled">';
         html += '          <li><a class="btn btn-success text-white" href="shop-single.html"><i class="far fa-heart"></i></a></li>';
-        html += '          <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="far fa-eye"></i></a></li>';
+        html += '          <li><a class="btn btn-success text-white mt-2" href="shop-single.html?id=' + p.id + '"><i class="far fa-eye"></i></a></li>';
         html += '          <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="fas fa-cart-plus"></i></a></li>';
         html += '        </ul>';
         html += '      </div>';
         html += '    </div>';
         html += '    <div class="card-body">';
-        html += '      <a href="shop-single.html" class="h3 text-decoration-none">' + p.title + '</a>';
+        html += '      <a href="shop-single.html?id=' + p.id + '" class="h3 text-decoration-none">' + p.title + '</a>';
         html += '      <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">';
         html += '        <li>' + p.sizes.join('/') + '</li>';
         html += '        <li class="pt-2">';
@@ -38,7 +38,7 @@ $(function () {
         html += '</li></ul>';
         html += '      <p class="text-center mb-0">$' + p.price + '</p>';
         html += '      <div class="text-center mt-2">';
-        html += '        <button class="btn btn-success btn-add-cart" data-id="' + p.id + '">Add to Cart</button>';
+        html += '        <button class="btn btn-success btn-add-cart" data-id="' + p.id + '" data-item-id="' + p.id + '" data-item-name="' + (p.title && p.title.replace(/"/g, '&quot;')) + '">Add to Cart</button>';
         html += '      </div>';
         html += '    </div>';
         html += '  </div>';
@@ -74,3 +74,70 @@ $(function () {
   });
 });
 
+// Render single product on shop-single.html when ?id=xxx
+$(function () {
+  var $productDetail = $('#product-detail');
+  if (!$productDetail.length) return;
+
+  var params = new URLSearchParams(window.location.search);
+  var id = parseInt(params.get('id'), 10);
+  if (!id) {
+    $('.container.pb-5').html('<div class="alert alert-warning">No product id provided.</div>');
+    return;
+  }
+
+  fetch('./data/products.json')
+    .then(function (res) { return res.json(); })
+    .then(function (products) {
+      var p = products.find(function (x) { return x.id === id; });
+      if (!p) {
+        $('.container.pb-5').html('<div class="alert alert-danger">Product not found.</div>');
+        return;
+      }
+
+      // Main image + thumbnails
+      $productDetail.attr('src', p.image).attr('alt', p.title);
+      $('#multi-item-example .carousel-inner img.card-img').attr('src', p.image).attr('alt', p.title);
+
+      // Title + Price
+      $('.card-body h1.h2').text(p.title);
+      $('.card-body p.h3.py-2').text('$' + p.price);
+
+      // Rating + reviews
+      var $ratingP = $('.card-body p.py-2').last();
+      var stars = '';
+      for (var i = 0; i < 5; i++) {
+        stars += i < p.rating ? '<i class="fa fa-star text-warning"></i>' : '<i class="fa fa-star text-secondary"></i>';
+      }
+      $ratingP.html(stars + ' <span class="list-inline-item text-dark">Rating ' + (p.rating).toFixed(1) + ' | ' + (p.reviews || 0) + ' Comments</span>');
+
+      // Hidden product-title
+      $('input[name="product-title"]').val(p.title);
+
+      // Sizes (replace the size buttons)
+      var $sizeUl = $('input[name="product-size"]').closest('li').parent();
+      $sizeUl.empty();
+      $sizeUl.append('<li class="list-inline-item">Size : <input type="hidden" name="product-size" id="product-size" value="' + (p.sizes[0] || '') + '"></li>');
+      p.sizes.forEach(function (s) {
+        $sizeUl.append('<li class="list-inline-item"><span class="btn btn-success btn-size">' + s + '</span></li>');
+      });
+      $sizeUl.on('click', '.btn-size', function () {
+        $('#product-size').val($(this).text());
+        $sizeUl.find('.btn-size').removeClass('active');
+        $(this).addClass('active');
+      });
+      $sizeUl.find('.btn-size').first().addClass('active');
+
+      // Available colors text
+      $('h6').filter(function () { return $(this).text().trim().startsWith('Avaliable Color'); })
+        .next().find('strong').text(p.colors.join(' / '));
+
+      // Make the single-page "Add To Cart" button work with existing handler
+      $('button[name="submit"][value="addtocard"]').addClass('btn-add-cart').attr('data-id', p.id);
+
+    })
+    .catch(function (err) {
+      console.error('Failed to load products.json', err);
+      $('.container.pb-5').html('<div class="alert alert-danger">Failed to load product data.</div>');
+    });
+});
